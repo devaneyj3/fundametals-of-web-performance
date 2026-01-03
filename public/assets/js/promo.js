6fe6287abc142afe8578c2d615f2c95f4b6e3963
devaneyj3 <jordandevaney28@gmail.com>
2026-01-03 12:26:05 -0500

    lazy load images

--- public/assets/js/promo.js
+++ public/assets/js/promo.js
@@ -10,37 +10,41 @@
  * 3. Shifting layouts
  */
 window.addEventListener("DOMContentLoaded", async () => {
+	const productsResp = await fetch(`${API_BASE_URL}/api/products`);
+	const products = await productsResp.json();
 
-  const productsResp = await fetch(`${API_BASE_URL}/api/products`);
-  const products = await productsResp.json();
+	const el = document.getElementById("promo-banner");
 
-  const el = document.getElementById("promo-banner");
-
-  let innerHTML = "<div class='container'><div class='promo-list flex-column'>"
-  products.forEach((product) => {
-    innerHTML += `
-      <div class="product-card ${product.isPromo ? 'promo' : ''}">
+	let innerHTML = "<div class='container'><div class='promo-list flex-column'>";
+	products.forEach((product) => {
+		innerHTML += `
+      <div class="product-card ${product.isPromo ? "promo" : ""}">
         <a href="/products/${product.slug}">
-          <img src="${STATIC_BASE_URL}${product.imagePath}?promo" alt="${product.name}" ${product.isPromo ? 'onload="showPromo()"' : ''} />
+          <img loading="lazy" src="${STATIC_BASE_URL}${
+			product.imagePath
+		}?promo" alt="${product.name}" ${
+			product.isPromo ? 'onload="showPromo()"' : ""
+		} />
           <div class="product-copy flex-column">
             <h2>Flash Sale!!</h2>
             <h3>${product.name}</h3>
           </div>
         </a>
         <div class="flex align-center">
-          <button type="button" class="add-to-cart btn-inverse btn-big" data-product-id="${product.id}">Add to Cart</button>
+          <button type="button" class="add-to-cart btn-inverse btn-big" data-product-id="${
+						product.id
+					}">Add to Cart</button>
         </div>
       </div>`;
-  });
-  innerHTML += "</div>"
-  innerHTML += "</div>";
-  el.innerHTML = innerHTML;
+	});
+	innerHTML += "</div>";
+	innerHTML += "</div>";
+	el.innerHTML = innerHTML;
 
-  // expand the promoted product
-  window.showPromo = function () {
-    setTimeout(() => {
-      el.querySelector(".product-card.promo").classList.add("expand");
-    }, 2000);
-  }
-
+	// expand the promoted product
+	window.showPromo = function () {
+		setTimeout(() => {
+			el.querySelector(".product-card.promo").classList.add("expand");
+		}, 2000);
+	};
 });
